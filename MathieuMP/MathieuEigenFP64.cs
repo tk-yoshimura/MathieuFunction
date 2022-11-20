42b923329123327559e2f435ea3cb354ec63badb
tk-yoshimura <yoshimura8192@gmail.com>
2022-11-20 13:25:01 +0900

    + search

--- MathieuMP/MathieuEigenFP64.cs
+++ MathieuMP/MathieuEigenFP64.cs
@@ -3,22 +3,25 @@
 
 namespace MathieuMP {
     public static class MathieuEigenFP64 {
+        // If a matches the true value, return 0.
         public static double FractionA(int n, double q, double a, int terms = 32) {
             if (n > 16384 || !(q >= 0)) {
                 throw new ArgumentException($"{nameof(n)}, {nameof(q)}");
             }
 
+            int n_sq = n * n;
+
             double h = q * q;
             double u = ((n & 1) == 0)
-                ? (n == 0) ? 0d : 2d / (a + (n * n))
-                : (n == 1) ? 1d / q : 1d / (a + (n * n - 1) - q);
+                ? (n == 0) ? 0d : 2d / (a + n_sq)
+                : (n == 1) ? 1d / q : 1d / (a + (n_sq - 1) - q);
             double v = 0d;
 
             for (int k = 2 + (n & 1); k < n; k += 2) {
-                u = 1d / (a + (n * n - k * k) - u * h);
+                u = 1d / (a + (n_sq - k * k) - u * h);
             }
             for (int k = terms * 2 + n; k > n; k -= 2) {
-                v = 1d / (a + (n * n - k * k) - v * h);
+                v = 1d / (a + (n_sq - k * k) - v * h);
             }
 
             double y = (((n > 0) ? u : v) + v) * h - a;
@@ -26,27 +29,100 @@ namespace MathieuMP {
             return y;
         }
 
+        // If a matches the true value, return 0.
         public static double FractionB(int n, double q, double a, int terms = 32) {
             if (n < 1 || n > 16384 || !(q >= 0)) {
                 throw new ArgumentException($"{nameof(n)}, {nameof(q)}");
             }
 
+            int n_sq = n * n;
+
             double h = q * q;
             double u = ((n & 1) == 0)
                 ? 0d
-                : (n == 1) ? -1d / q : 1d / (a + (n * n - 1) + q);
+                : (n == 1) ? -1d / q : 1d / (a + (n_sq - 1) + q);
             double v = 0d;
 
             for (int k = 2 + (n & 1); k < n; k += 2) {
-                u = 1d / (a + (n * n - k * k) - u * h);
+                u = 1d / (a + (n_sq - k * k) - u * h);
             }
             for (int k = terms * 2 + n; k > n; k -= 2) {
-                v = 1d / (a + (n * n - k * k) - v * h);
+                v = 1d / (a + (n_sq - k * k) - v * h);
             }
 
             double y = (u + v) * h - a;
 
             return y;
+        }
+
+        // The true value is obtained by the secant method.
+        public static (double value, double error) SearchFitA(int n, double q, double a, int maxiter = 16) {
+            if (q == 0) {
+                return (0d, 0d);
+            }
+
+            double a0 = a, d0 = FractionA(n, q, a0), da = double.NaN;
+
+            for (int iter = 0; iter < maxiter; iter++) {
+                double ah = Math.CopySign(Math.Max(Math.ScaleB(1, -40), Math.ScaleB(Math.Abs(a0), -10)), a0);
+                double a1 = a0 + ah, d1 = FractionA(n, q, a1);
+
+                double dh = d1 - d0;
+
+                if (dh == 0) {
+                    break;
+                }
+
+                da = ah / dh * d0;
+                a0 -= da;
+
+                if (Math.Abs(da / a0) <= 1e-15) {
+                    break;
+                }
+
+                d0 = FractionA(n, q, a0);
+
+                if (d0 == 0) {
+                    break;
+                }
+            }
+
+            return (a0, da);
+        }
+
+        // The true value is obtained by the secant method.
+        public static (double value, double error) SearchFitB(int n, double q, double a, int maxiter = 16) {
+            if (q == 0) {
+                return (0d, 0d);
+            }
+
+            double a0 = a, d0 = FractionB(n, q, a0), da = double.NaN;
+
+            for (int iter = 0; iter < maxiter; iter++) {
+                double ah = Math.CopySign(Math.Max(Math.ScaleB(1, -40), Math.ScaleB(Math.Abs(a0), -10)), a0);
+                double a1 = a0 + ah, d1 = FractionB(n, q, a1);
+
+                double dh = d1 - d0;
+
+                if (dh == 0) {
+                    break;
+                }
+
+                da = ah / dh * d0;
+                a0 -= da;
+
+                if (Math.Abs(da / a0) <= 1e-15) {
+                    break;
+                }
+
+                d0 = FractionB(n, q, a0);
+
+                if (d0 == 0) {
+                    break;
+                }
+            }
+
+            return (a0, da);
         }
     }
 }
--- MathieuMP/Program.cs
+++ MathieuMP/Program.cs
@@ -3,11 +3,19 @@
 namespace MathieuMP {
     class Program {
         static void Main() {
-            for (double a = -16; a <= 16; a += 1d / 32) {
-                double d = MathieuEigenFP64.FractionA(3, 4, a);
+            //for (double a = -16; a <= 16; a += 1d / 32) {
+            //    double d = MathieuEigenFP64.FractionB(3, 4, a);
+            //
+            //    Console.WriteLine($"{a},{d}");
+            //}
 
-                Console.WriteLine($"{a},{d}");
+            for (int n = 1; n <= 16; n++) {
+                (double y, double err) = MathieuEigenFP64.SearchFitB(n, Math.ScaleB(1, -400), 0);
+            
+                Console.WriteLine(y);
+                Console.WriteLine(err);
             }
+
 
             Console.WriteLine("END");
             Console.Read();
