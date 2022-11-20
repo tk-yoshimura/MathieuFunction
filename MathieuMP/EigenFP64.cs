bac9a98a309383eaf3a9423b6a9dec2205d70448
tk-yoshimura <yoshimura8192@gmail.com>
2022-11-20 23:44:26 +0900

    + initvalue

--- MathieuMP/EigenFP64.cs
+++ MathieuMP/EigenFP64.cs
@@ -63,7 +63,7 @@ namespace MathieuMP {
         }
 
         // The true value is obtained by the secant method.
-        public static (double value, double error) SearchFit(EigenFunc func, int n, double q, double a, int maxiter = 16) {
+        private static (double value, double error) SearchFit(EigenFunc func, int n, double q, double a, int maxiter = 16) {
             if (q == 0) {
                 return (0d, 0d);
             }
@@ -79,7 +79,6 @@ namespace MathieuMP {
                 if (dh == 0) {
                     break;
                 }
-
                 da = ah / dh * d0;
                 a0 -= da;
 
@@ -87,14 +86,95 @@ namespace MathieuMP {
                     break;
                 }
 
+                Console.WriteLine($"{ah/dh},{d0 - d1},{a0}");
+
                 d0 = Fraction(func, n, q, a0);
 
                 if (d0 == 0) {
-                    break;
+                    return (a0, 0d);
                 }
             }
 
             return (a0, da);
+        }
+
+        // Initial value for the secant method.
+        public static double InitialValue(EigenFunc func, int n, double q) {
+            if (!Enum.IsDefined(func)) {
+                throw new ArgumentException(nameof(func));
+            }
+
+            if (func == EigenFunc.A && n < 0) {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+            if (func == EigenFunc.B && n < 1) {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            double y = (q < 2 * Math.Max(1, n * n)) ? NearZero(func, n, q) : Asymptotic(func, n, q);
+            y = Math.Min(y, UpperBound(func, n, q));
+
+            return y;
+        }
+
+        private static double NearZero(EigenFunc func, int n, double q) {
+            double h = q * q;
+
+            double y = (func, n) switch {
+                (EigenFunc.A, 0) => h * (-1d / 2),
+                (EigenFunc.A, 1) => q + h * (-1d / 8),
+                (EigenFunc.B, 1) => -q + h * (-1d / 8),
+                (EigenFunc.A, 2) => h * (5d / 12),
+                (EigenFunc.B, 2) => h * (-1d / 12),
+                _ => h / (2 * (n * n - 1))
+            };
+
+            return y;
+        }
+
+        private static double Asymptotic(EigenFunc func, int n, double q) {
+            double u = Math.Sqrt(q), v = 1d / u;
+            double s = func == EigenFunc.A ? (2 * n + 1) : (2 * n - 1), s_sq = s * s;
+
+            double c0 = -Math.ScaleB(1 + s_sq, -3);
+            double c1 = -Math.ScaleB(s * (3 + s_sq), -7);
+            double c2 = -Math.ScaleB(9 + s_sq * (34 + s_sq * 5), -12);
+            double c3 = -Math.ScaleB(s * (405 + s_sq * (410 + s_sq * 33)), -17);
+            double c4 = -Math.ScaleB(486 + s_sq * (2943 + s_sq * (1260 + s_sq * 63)), -20);
+            double c5 = -Math.ScaleB(s * (41607 + s_sq * (69001 + s_sq * (15617 + s_sq * 527))), -25);
+
+            double y = (2 * (-q + s * u) - n * n) + c0 + v * (c1 + v * (c2 + v * (c3 + v * (c4 + v * c5))));
+
+            return y;
+        }
+
+        private static double UpperBound(EigenFunc func, int n, double q) {
+            double maxrate = (func, n) switch {
+                (EigenFunc.A, 0) => 0,
+                (EigenFunc.A, 1) => 1.52169,
+                (EigenFunc.A, 2) => 1.03126,
+                (EigenFunc.A, _) => 0.846,
+                (EigenFunc.B, <= 2) => 0,
+                (EigenFunc.B, 3) => 0.0291174,
+                (EigenFunc.B, 4) => 0.0883491,
+                (EigenFunc.B, _) => 0.4,
+                (_, _) => throw new ArgumentException(nameof(func))
+            };
+
+            double y = n * n * maxrate;
+
+            return y;
+        }
+
+        public static (double value, double error) Value(EigenFunc func, int n, double q, bool zero_shift = false) {
+            double a0 = InitialValue(func, n, q);
+            (double value, double error) = SearchFit(func, n, q, a0);
+
+            if (!zero_shift) {
+                value += n * n;
+            }
+
+            return (value, error);
         }
     }
 }
--- MathieuMP/Program.cs
+++ MathieuMP/Program.cs
@@ -3,24 +3,32 @@
 namespace MathieuMP {
     class Program {
         static void Main() {
-            //for (double a = -16; a <= 16; a += 1d / 32) {
-            //    double d = MathieuEigenFP64.FractionB(3, 4, a);
+
+            //for (int n = 0; n <= 16; n++) {
+            //    Console.WriteLine($"n={n}");
+            //    for (double q = 0, h = 16 * Math.Max(1, n * n); q <= h; q += h / 64) {
+            //        double init = EigenFP64.InitialValue(EigenFunc.A, n, q);
+            //        (double value, double err) = EigenFP64.Value(EigenFunc.A, n, q, zero_shift: true);
             //
-            //    Console.WriteLine($"{a},{d}");
+            //        Console.WriteLine($"{q},{init},{value},{err}");
+            //    }
             //}
 
-            for (int n = 1; n <= 16; n++) {
-                (double y, double err) = EigenFP64.SearchFit(EigenFunc.B, n, 2, 0);
-            
-                Console.WriteLine(y);
-                Console.WriteLine(err);
-            }
+            EigenFP64.Value(EigenFunc.A, 4, 15, zero_shift: true);
+            Console.WriteLine("");
 
-            for (int n = 0; n <= 16; n++) {
-                (double y, double err) = EigenFP64.SearchFit(EigenFunc.A, n, 2, 0);
-            
-                Console.WriteLine(y);
-                Console.WriteLine(err);
+            EigenFP64.Value(EigenFunc.A, 4, 16, zero_shift: true);
+            Console.WriteLine("");
+
+            EigenFP64.Value(EigenFunc.A, 4, 17, zero_shift: true);
+            Console.WriteLine("");
+
+            EigenFP64.Value(EigenFunc.A, 4, 18, zero_shift: true);
+            Console.WriteLine("");
+
+            for (double a = 0; a <= 16; a += 1d / 64) {
+                double y = EigenFP64.Fraction(EigenFunc.A, 4, 16, a);
+                Console.WriteLine($"{a},{y}");
             }
 
 
