ebaf676b77d73c033b017d2ebcbf0fc869442a93
tk-yoshimura <yoshimura8192@gmail.com>
2022-12-31 21:47:04 +0900

    Update Program.cs

--- MathieuMP/Program.cs
+++ MathieuMP/Program.cs
@@ -3,41 +3,109 @@
 namespace MathieuMP {
     class Program {
         static void Main() {
-            foreach (EigenFunc func in new[] { EigenFunc.A, EigenFunc.B }) {
-                FracTerms<Pow2.N32>(func);
+            List<MultiPrecision<Pow2.N8>> us = new();
+
+            for (MultiPrecision<Pow2.N8> u = MultiPrecision<Pow2.N8>.Ldexp(1, -64); u < 1 / 1024d; u *= 2) {
+                us.Add(u);
+            }
+            for (MultiPrecision<Pow2.N8> u = 1 / 1024d; u < 1; u += 1 / 1024d) {
+                us.Add(u);
+            }
+            for (MultiPrecision<Pow2.N8> u = 1; u < 4; u += 1 / 256d) { 
+                us.Add(u);
+            }
+            for (MultiPrecision<Pow2.N8> u = 4; u < 16; u += 1 / 64d) { 
+                us.Add(u);
+            }
+            for (MultiPrecision<Pow2.N8> u = 16; u < 64; u += 1 / 16d) { 
+                us.Add(u);
+            }
+            for (MultiPrecision<Pow2.N8> u = 64; u < 256; u += 1 / 4d) { 
+                us.Add(u);
+            }
+            for (MultiPrecision<Pow2.N8> u = 256; u <= 1024; u += 1) { 
+                us.Add(u);
+            }
+
+            for (int n = 0; n <= 64; n++) {
+                Console.WriteLine($"Plotting {n}");
+
+                using StreamWriter sw = new($"../../../../results/eigen_precision40_n{n}.csv");
+                sw.WriteLine($"# zero shifted mathieu eigen value precision_digits=40 n={n}");
+                sw.WriteLine("# u:=q^2/max(1, n^4), mean:=(a+b)/2, diff:=(a-b)/2, scaled_diff:=diff*(2^(n-1)*(n-1)!)^2/q^n");
+                sw.WriteLine("u,a,b,mean,diff,scaled_diff,digits_loss(1/0)");
+
+                int s = Math.Max(1, n * n);
+                MultiPrecision<Pow2.N8> r = MultiPrecision<Pow2.N8>.Ldexp(MultiPrecision<Pow2.N8>.Square(MultiPrecision<Pow2.N8>.Gamma(n)), 2 * n - 2);
+
+                if (n >= 1) {
+                    sw.WriteLine("0,0,0,0,0,1,0");
+                }
+                else {
+                    sw.WriteLine("0,0,0,0,0,0,0");
+                }
+
+                foreach (MultiPrecision<Pow2.N8> u in us) {
+                    MultiPrecision<Pow2.N8> q = s * MultiPrecision<Pow2.N8>.Sqrt(u);
+
+                    (MultiPrecision<Pow2.N8> a, MultiPrecision<Pow2.N8> b, MultiPrecision<Pow2.N8> m, MultiPrecision<Pow2.N8> d, bool cancellation_digits)
+                        = ComputeDigits40(n, q);
+
+                    MultiPrecision<Pow2.N8> sd = r * d / MultiPrecision<Pow2.N8>.Pow(q, n);
+
+                    if (n >= 1) {
+                        sw.WriteLine($"{u},{a:e40},{b:e40},{m:e40},{d:e40},{sd:e40},{(cancellation_digits ? "1" : "0")}");
+                        Console.WriteLine($"{u},{a:e40},{sd:e40}");
+                    }
+                    else { 
+                        sw.WriteLine($"{u},{a:e40},0,0,0,0,0");
+                        Console.WriteLine($"{u},{a:e40}");
+                    }
+                }
             }
 
             Console.WriteLine("END");
             Console.Read();
         }
 
-        private static void FracTerms<N>(EigenFunc func) where N: struct, IConstant {
-            int terms_i0 = 0;
+        static (MultiPrecision<N> a, MultiPrecision<N> b, MultiPrecision<N> m, MultiPrecision<N> d, bool cancellation_digits) Compute<N, M>(int n, MultiPrecision<M> q, int needs_bits)
+            where N: struct, IConstant where M : struct, IConstant {
 
-            for (int n = func == EigenFunc.A ? 0 : 1; n <= 32; n++) {
-                Console.WriteLine($"{func}{n}");
-
-                using StreamWriter sw = new($"../../../../sandbox/needs_frac_log2_mp{MultiPrecision<N>.Length}_{func}_{n}.csv");
-
-                int terms = (terms_i0 <= 0) ? 1 : terms_i0;
-                double terms_rate = 1;
-
-                for ((int i, MultiPrecision<N> q) = (0, Math.Max(1, n * n) / 8192d); q <= Math.Max(1, n * n) * 1024d; i++, q *= 2) {
-                    int terms_init = Math.Max(1, (int)(terms * terms_rate) - 3);
-
-                    (MultiPrecision<N> a, int terms_res) = EigenMP<N>.ConvergenceFracTerms(func, n, q, terms_init);
-
-                    terms_rate = (i > 0) ? (double)terms_res / terms : 1;
-                    terms = terms_res;
-
-                    if (i <= 0) {
-                        terms_i0 = terms_res;
-                    }
-
-                    Console.WriteLine($"{q},{a},{terms_res},{terms_init},{terms_rate}{((terms_res - terms_init < 1) ? ",*" : "")}");
-                    sw.WriteLine($"{q},{a},{terms_res}");
-                }
+            if (MultiPrecision<N>.Length > MultiPrecision<M>.Length) {
+                throw new ArgumentException("Invalid multi precision length.");
             }
+        
+            MultiPrecision<M> a = EigenMP<M>.Value(EigenFunc.A, n, q, zero_shift: true).value;
+            MultiPrecision<M> b = EigenMP<M>.Value(EigenFunc.B, n, q, zero_shift: true).value;
+            MultiPrecision<M> m = (a + b) / 2;
+            MultiPrecision<M> d = (a - b) / 2;
+
+            long loss_bits = Math.Max(a.Exponent, b.Exponent) - d.Exponent;
+            bool cancellation_digits = loss_bits >= MultiPrecision<M>.Bits - needs_bits;
+
+            return (a.Convert<N>(), b.Convert<N>(), m.Convert<N>(), d.Convert<N>(), cancellation_digits);
+        }
+
+        static (MultiPrecision<Pow2.N8> a, MultiPrecision<Pow2.N8> b, MultiPrecision<Pow2.N8> m, MultiPrecision<Pow2.N8> d, bool cancellation_digits) ComputeDigits40(int n, MultiPrecision<Pow2.N8> q) {
+            if (n == 0) {
+                return (EigenMP<Pow2.N8>.Value(EigenFunc.A, n, q, zero_shift: true).value, 0, 0, 0, cancellation_digits: false);
+            }
+            
+            MultiPrecision<Pow2.N8> a, b, m, d;
+            bool cancellation_digits;
+
+            (a, b, m, d, cancellation_digits) = Compute<Pow2.N8, Pow2.N8>(n, q, needs_bits: 144);
+            if (!cancellation_digits) {
+                return (a, b, m, d, cancellation_digits);
+            }
+
+            (a, b, m, d, cancellation_digits) = Compute<Pow2.N8, Pow2.N16>(n, q.Convert<Pow2.N16>(), needs_bits: 144);
+            if (!cancellation_digits) {
+                return (a, b, m, d, cancellation_digits);
+            }
+
+            (a, b, m, d, cancellation_digits) = Compute<Pow2.N8, Pow2.N32>(n, q.Convert<Pow2.N32>(), needs_bits: 144);
+            return (a, b, m, d, cancellation_digits);
         }
     }
 }
