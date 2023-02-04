da02f67ca59c00c03699b1f0016209b48840dd38
tk-yoshimura <yoshimura8192@gmail.com>
2023-02-05 03:28:19 +0900

    fix ddouble pade

--- MathieuDDouble/Program.cs
+++ MathieuDDouble/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DoubleDouble;
+using MultiPrecision;
 using System;
 using System.Text;
 
@@ -19,7 +20,9 @@ namespace MathieuPadeDDouble {
                 (64, 1024) ,
             };
 
-            for (int n = 0; n <= 32; n++) {
+            using StreamWriter sw_com = new($"../../../../sandbox/eigen_ddouble_padecoef.txt");
+
+            for (int n = 0; n <= 16; n++) {
                 using StreamWriter sw = new($"../../../../results/ddouble/eigen_ddouble_results_m_n{n}.csv");
                 sw.WriteLine("x,expected,approx,denom,error,relative_error");
 
@@ -27,7 +30,7 @@ namespace MathieuPadeDDouble {
                     List<(ddouble u, ddouble m, ddouble d)> expected = ReadExpected(n, umin, umax);
                     using StreamReader sr = new($"../../../../results/padecoef/eigen_padecoef_precisionbits104_range{umin}to{umax}_m_n{n}.csv");
 
-                    ((ddouble[] ms, ddouble[] ns), ddouble u0) = ReadPadecoef(sr, n, umin, umax);
+                    ((ddouble[] ms, ddouble[] ns), ddouble u0) = ReadPadecoef(sr);
 
                     PlotResult(
                         sw,
@@ -39,7 +42,25 @@ namespace MathieuPadeDDouble {
                 }
             }
 
-            for (int n = 1; n <= 32; n++) {
+            for (int n = 0; n <= 16; n++) {
+                sw_com.WriteLine($"public static ReadOnlyCollection<ReadOnlyCollection<(ddouble c, ddouble d)>> PadeM{n}Tables = Array.AsReadOnly(new ReadOnlyCollection<(ddouble c, ddouble d)>[] {{");
+
+                foreach ((ddouble umin, ddouble umax) in ranges) {
+                    using StreamReader sr = new($"../../../../results/padecoef/eigen_padecoef_precisionbits104_range{umin}to{umax}_m_n{n}.csv");
+
+                    ((MultiPrecision<Pow2.N8>[] ms, MultiPrecision<Pow2.N8>[] ns), MultiPrecision<Pow2.N8> u0) = ReadPadecoef<Pow2.N8>(sr);
+
+                    sw_com.WriteLine($"    new ReadOnlyCollection<(ddouble c, ddouble d)>(new (ddouble c, ddouble d)[] {{");
+                    for (int i = 0; i < ms.Length; i++) {
+                        sw_com.WriteLine($"        ({ToFP128(ms[i])}, {ToFP128(ns[i])}), ");
+                    }
+                    sw_com.WriteLine("    }),");
+                }
+
+                sw_com.WriteLine("});");
+            }
+
+            for (int n = 1; n <= 16; n++) {
                 using StreamWriter sw = new($"../../../../results/ddouble/eigen_ddouble_results_d_n{n}.csv");
                 sw.WriteLine("x,expected,approx,denom,error,relative_error");
 
@@ -47,7 +68,7 @@ namespace MathieuPadeDDouble {
                     List<(ddouble u, ddouble m, ddouble d)> expected = ReadExpected(n, umin, umax);
                     using StreamReader sr = new($"../../../../results/padecoef/eigen_padecoef_precisionbits104_range{umin}to{umax}_d_n{n}.csv");
 
-                    ((ddouble[] ms, ddouble[] ns), ddouble u0) = ReadPadecoef(sr, n, umin, umax);
+                    ((ddouble[] ms, ddouble[] ns), ddouble u0) = ReadPadecoef(sr);
 
                     PlotResult(
                         sw,
@@ -57,6 +78,24 @@ namespace MathieuPadeDDouble {
                         ms, ns
                     );
                 }
+            }
+
+            for (int n = 1; n <= 16; n++) {
+                sw_com.WriteLine($"public static ReadOnlyCollection<ReadOnlyCollection<(ddouble c, ddouble d)>> PadeD{n}Tables = Array.AsReadOnly(new ReadOnlyCollection<(ddouble c, ddouble d)>[] {{");
+
+                foreach ((ddouble umin, ddouble umax) in ranges) {
+                    using StreamReader sr = new($"../../../../results/padecoef/eigen_padecoef_precisionbits104_range{umin}to{umax}_d_n{n}.csv");
+
+                    ((MultiPrecision<Pow2.N8>[] ms, MultiPrecision<Pow2.N8>[] ns), MultiPrecision<Pow2.N8> u0) = ReadPadecoef<Pow2.N8>(sr);
+
+                    sw_com.WriteLine($"    new ReadOnlyCollection<(ddouble c, ddouble d)>(new (ddouble c, ddouble d)[] {{");
+                    for (int i = 0; i < ms.Length; i++) {
+                        sw_com.WriteLine($"        ({ToFP128(ns[i])}, {ToFP128(ms[i])}), ");
+                    }
+                    sw_com.WriteLine("    }),");
+                }
+
+                sw_com.WriteLine("});");
             }
 
             Console.WriteLine("END");
@@ -98,7 +137,7 @@ namespace MathieuPadeDDouble {
             return res;
         }
 
-        static ((ddouble[] ms, ddouble[] ns) padecoef, ddouble u0) ReadPadecoef(StreamReader sr, int n, ddouble umin, ddouble umax) {
+        static ((ddouble[] ms, ddouble[] ns) padecoef, ddouble u0) ReadPadecoef(StreamReader sr) {
             List<ddouble> ms = new(), ns = new();
 
             string line = sr.ReadLine();
@@ -107,6 +146,43 @@ namespace MathieuPadeDDouble {
             }
 
             ddouble u0 = line[5..];
+
+            if (!(sr.ReadLine().StartsWith("numers"))) {
+                throw new FormatException();
+            }
+
+            while (!sr.EndOfStream) {
+                line = sr.ReadLine();
+
+                if (line.StartsWith("denoms")) {
+                    break;
+                }
+
+                ms.Add(line);
+            }
+
+            while (!sr.EndOfStream) {
+                line = sr.ReadLine();
+
+                if (line == "u,expected,approx,error") {
+                    break;
+                }
+
+                ns.Add(line);
+            }
+
+            return ((ms.ToArray(), ns.ToArray()), u0);
+        }
+
+        static ((MultiPrecision<N>[] ms, MultiPrecision<N>[] ns) padecoef, MultiPrecision<N> u0) ReadPadecoef<N>(StreamReader sr) where N: struct, IConstant {
+            List<MultiPrecision<N>> ms = new(), ns = new();
+
+            string line = sr.ReadLine();
+            if (!(line.StartsWith("u0 = "))) {
+                throw new FormatException();
+            }
+
+            MultiPrecision<N> u0 = line[5..];
 
             if (!(sr.ReadLine().StartsWith("numers"))) {
                 throw new FormatException();
@@ -166,6 +242,16 @@ namespace MathieuPadeDDouble {
 
                 sw.WriteLine($"{x},{expected},{approx},{d},{error},{relative_error}");
             }
+        }
+
+        public static string ToFP128<N>(MultiPrecision<N> x) where N : struct, IConstant {
+            Sign sign = x.Sign;
+            long exponent = x.Exponent;
+            uint[] mantissa = x.Mantissa.Reverse().ToArray();
+
+            string code = $"({(sign == Sign.Plus ? "+1" : "-1")}, {exponent}, 0x{mantissa[0]:X8}{mantissa[1]:X8}uL, 0x{mantissa[2]:X8}{mantissa[3]:X8}uL)";
+
+            return code;
         }
     }
 }
