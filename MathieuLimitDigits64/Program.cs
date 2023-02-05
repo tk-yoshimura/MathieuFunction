6aee8c9c45732c0318027ff84a4017424a9983b7
tk-yoshimura <yoshimura8192@gmail.com>
2023-02-06 03:22:08 +0900

    + limit

---
+++ MathieuLimitDigits64/Program.cs
@@ -0,0 +1,215 @@
+﻿using MultiPrecision;
+using System.Numerics;
+using static MultiPrecision.Pow2;
+
+namespace MathieuMP {
+    class Program {
+        static void Main() {
+            List<MultiPrecision<N8>> us = new();
+
+            for (MultiPrecision<N8> u = 1024; u.Exponent <= 40; u *= 2) {
+                for (MultiPrecision<N8> f = 1; f < 2; f += 1d / 16) {
+                    us.Add(u * f);
+                }
+            }
+
+            for (int n = 0; n <= 16; n++) {
+                Console.WriteLine($"Plotting {n}");
+
+                using StreamWriter sw = new($"../../../../results/eigen_limit_r2_precision64_n{n}.csv");
+                sw.WriteLine($"# zero shifted mathieu eigen limit value precision_digits=64 n={n}");
+
+                if (n >= 1) {
+                    sw.WriteLine("# u:=q^2/n^4");
+                }
+                else {
+                    sw.WriteLine("# u:=q^2");
+                }
+
+                sw.WriteLine("u,a,a_limit,a_delta,b,b_limit,b_delta,digits_loss(1/0)");
+
+                int s = Math.Max(1, n * n);
+                int i = 0, mp_length = 0;
+
+                foreach (MultiPrecision<N8> u in us) {
+                    MultiPrecision<N64> q = s * MultiPrecision<N64>.Sqrt(u.Convert<N64>());
+
+                    (MultiPrecision<N8> a, MultiPrecision<N8> a_limit, MultiPrecision<N8> a_delta, MultiPrecision<N8> b, MultiPrecision<N8> b_limit, MultiPrecision<N8> b_delta, bool cancellation_digits, mp_length)
+                        = ComputeDigits64(n, q, mp_length);
+
+                    if (n >= 1) {
+                        sw.WriteLine($"{u},{a:e64},{a_limit:e64},{a_delta:e64},{b:e64},{b_limit:e64},{b_delta:e64},{(cancellation_digits ? "1" : "0")}");
+                        Console.WriteLine($"{u},{a:e20},{a_limit:e20},{b:e20},{b_limit:e20}");
+                    }
+                    else {
+                        sw.WriteLine($"{u},{a:e64},{a_limit:e64},{a_delta:e64},0,0,0,0");
+                        Console.WriteLine($"{u},{a:e20},{a_limit:e20}");
+                    }
+
+                    sw.Flush();
+
+                    i++;
+                    if ((i % 8) == 0) {
+                        mp_length -= 4;
+                    }
+                }
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+
+        static (MultiPrecision<N> a, MultiPrecision<N> a_limit, MultiPrecision<N> a_delta, MultiPrecision<N> b, MultiPrecision<N> b_limit, MultiPrecision<N> b_delta, bool cancellation_digits) Compute<N, M>(int n, MultiPrecision<M> q, int needs_bits)
+            where N : struct, IConstant where M : struct, IConstant {
+
+            if (MultiPrecision<N>.Length > MultiPrecision<M>.Length) {
+                throw new ArgumentException("Invalid multi precision length.");
+            }
+            MultiPrecision<M> a = EigenMP<M>.Value(EigenFunc.A, n, q, zero_shift: true).value;
+            MultiPrecision<M> a_limit = LimitEigenA(n, q);
+            MultiPrecision<M> a_delta = a_limit - a;
+
+            MultiPrecision<M> b = (n > 0) ? EigenMP<M>.Value(EigenFunc.B, n, q, zero_shift: true).value : 0;
+            MultiPrecision<M> b_limit = (n > 0) ? LimitEigenB(n, q) : 0;
+            MultiPrecision<M> b_delta = b_limit - b;
+
+            long loss_bits_a = Math.Max(0, Math.Max(a.Exponent, a_limit.Exponent) - a_delta.Exponent);
+            long loss_bits_b = Math.Max(0, Math.Max(b.Exponent, b_limit.Exponent) - b_delta.Exponent);
+            long loss_bits = Math.Max(loss_bits_a, loss_bits_b);
+
+            bool cancellation_digits = loss_bits >= MultiPrecision<M>.Bits - needs_bits;
+
+            Console.WriteLine($"loss_bits: {loss_bits}");
+
+            return (a.Convert<N>(), a_limit.Convert<N>(), a_delta.Convert<N>(),
+                    b.Convert<N>(), b_limit.Convert<N>(), b_delta.Convert<N>(), cancellation_digits);
+        }
+
+        static (MultiPrecision<N8> a, MultiPrecision<N8> a_limit, MultiPrecision<N8> a_delta, MultiPrecision<N8> b, MultiPrecision<N8> b_limit, MultiPrecision<N8> b_delta, bool cancellation_digits, int mp_length) ComputeDigits64(int n, MultiPrecision<N64> q, int mp_length) {
+            const int needs_bits = 228;
+
+            MultiPrecision<N8> a, a_limit, a_delta, b, b_limit, b_delta;
+            bool cancellation_digits;
+
+            if (mp_length <= 8) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, N8>(n, q.Convert<N8>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 8);
+                }
+            }
+
+            if (mp_length <= 12) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus4<N8>>(n, q.Convert<Plus4<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 12);
+                }
+            }
+
+            if (mp_length <= 16) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus8<N8>>(n, q.Convert<Plus8<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 16);
+                }
+            }
+
+            if (mp_length <= 20) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus12<N8>>(n, q.Convert<Plus12<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 20);
+                }
+            }
+
+            if (mp_length <= 24) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus16<N8>>(n, q.Convert<Plus16<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 24);
+                }
+            }
+
+            if (mp_length <= 28) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus20<N8>>(n, q.Convert<Plus20<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 28);
+                }
+            }
+
+            if (mp_length <= 32) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus24<N8>>(n, q.Convert<Plus24<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 32);
+                }
+            }
+
+            if (mp_length <= 36) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus28<N8>>(n, q.Convert<Plus28<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 36);
+                }
+            }
+
+            if (mp_length <= 40) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus32<N8>>(n, q.Convert<Plus32<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 40);
+                }
+            }
+
+            if (mp_length <= 44) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus36<N8>>(n, q.Convert<Plus36<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 44);
+                }
+            }
+
+            if (mp_length <= 48) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus40<N8>>(n, q.Convert<Plus40<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 48);
+                }
+            }
+
+            if (mp_length <= 52) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus44<N8>>(n, q.Convert<Plus44<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 52);
+                }
+            }
+
+            if (mp_length <= 56) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus48<N8>>(n, q.Convert<Plus48<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 56);
+                }
+            }
+
+            if (mp_length <= 60) {
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus52<N8>>(n, q.Convert<Plus52<N8>>(), needs_bits);
+                if (!cancellation_digits) {
+                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 60);
+                }
+            }
+
+            (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, N64>(n, q.Convert<N64>(), needs_bits);
+            if (!cancellation_digits) {
+                return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 64);
+            }
+
+            (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, N128>(n, q.Convert<N128>(), needs_bits);
+            return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 64);
+        }
+
+        static MultiPrecision<N> LimitEigenA<N>(int n, MultiPrecision<N> q) where N : struct, IConstant {
+            int s = 2 * n + 1;
+            MultiPrecision<N> y = 2 * (s * MultiPrecision<N>.Sqrt(q) - q) - MultiPrecision<N>.Div(6 * n * n + 2 * n + 1, 4);
+
+            return y;
+        }
+
+        static MultiPrecision<N> LimitEigenB<N>(int n, MultiPrecision<N> q) where N : struct, IConstant {
+            int s = 2 * n - 1;
+            MultiPrecision<N> y = 2 * (s * MultiPrecision<N>.Sqrt(q) - q) - MultiPrecision<N>.Div(6 * n * n - 2 * n + 1, 4);
+
+            return y;
+        }
+    }
+}
---
+++ MathieuPadeLimitApproximate/Program.cs
@@ -0,0 +1,191 @@
+﻿using MultiPrecision;
+using MultiPrecisionAlgebra;
+using static MultiPrecision.Pow2;
+
+namespace MathieuPadeApproximate {
+    class Program {
+        static void Main() {
+            for (int n = 0; n <= 16; n++) {
+                SearchAndPlotA(n);
+            }
+
+            for (int n = 1; n <= 16; n++) {
+                SearchAndPlotB(n);
+            }
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+
+        private static void SearchAndPlotA(int n) {
+            List<(MultiPrecision<N32> u, MultiPrecision<N32> a)> expecteds = ReadAExpected<N32>(n);
+
+            MultiPrecision<N32>[] a_limit = Vector<N32>.Func(
+                expecteds.Select(expected => expected.u).ToArray(),
+                u => LimitEigenA(n, MultiPrecision<N32>.Sqrt(u) * Math.Max(1, n * n))
+            );
+
+            List<(MultiPrecision<N32> u, MultiPrecision<N32> a, MultiPrecision<N32> a_limit)> expecteds_transform = new();
+            for (int i = 0; i < expecteds.Count; i++) {
+                expecteds_transform.Add((1 / expecteds[i].u, expecteds[i].a, a_limit[i]));
+            }
+
+            expecteds_transform.Reverse();
+
+            Vector<N32> parameter, approx;
+            bool success = false;
+
+            for (int k = 4; k <= 1024; k++) {
+                int numer = k + 1, denom = k;
+
+                Console.WriteLine($"numer {numer} denom {denom}");
+
+                (parameter, approx, success) = PadeApproximate<N32>(expecteds_transform, numer, denom);
+
+                if (success) {
+                    using StreamWriter sw = new($"../../../../results/padecoef/eigen_limit_padecoef_precisionbits104_quad_delta_a_n{n}.csv");
+                    PlotResult(sw, expecteds_transform, numer, parameter, approx);
+                    break;
+                }
+            }
+        }
+
+        private static void SearchAndPlotB(int n) {
+            List<(MultiPrecision<N32> u, MultiPrecision<N32> b)> expecteds = ReadBExpected<N32>(n);
+
+            MultiPrecision<N32>[] b_limit = Vector<N32>.Func(
+                expecteds.Select(expected => expected.u).ToArray(),
+                u => LimitEigenB(n, MultiPrecision<N32>.Sqrt(u) * Math.Max(1, n * n))
+            );
+
+            List<(MultiPrecision<N32> u, MultiPrecision<N32> b, MultiPrecision<N32> b_limit)> expecteds_transform = new();
+            for (int i = 0; i < expecteds.Count; i++) {
+                expecteds_transform.Add((1 / expecteds[i].u, expecteds[i].b, b_limit[i]));
+            }
+
+            expecteds_transform.Reverse();
+
+            Vector<N32> parameter, approx;
+            bool success = false;
+
+            for (int k = 4; k <= 1024; k++) {
+                int numer = k + 1, denom = k;
+
+                Console.WriteLine($"numer {numer} denom {denom}");
+
+                (parameter, approx, success) = PadeApproximate<N32>(expecteds_transform, numer, denom);
+
+                if (success) {
+                    using StreamWriter sw = new($"../../../../results/padecoef/eigen_limit_padecoef_precisionbits104_quad_delta_b_n{n}.csv");
+                    PlotResult(sw, expecteds_transform, numer, parameter, approx);
+                    break;
+                }
+            }
+        }
+
+        private static void PlotResult<N>(StreamWriter sw, List<(MultiPrecision<N32> u, MultiPrecision<N32> v, MultiPrecision<N32> v_limit)> expecteds, int numer, Vector<N> parameter, Vector<N> approx) where N : struct, IConstant {
+            sw.WriteLine($"numers: {numer}");
+            foreach ((_, MultiPrecision<N> v) in parameter[..numer]) {
+                sw.WriteLine($"{v:e64}");
+            }
+
+            sw.WriteLine($"denoms: {(parameter.Dim - numer)}");
+            foreach ((_, MultiPrecision<N> v) in parameter[numer..]) {
+                sw.WriteLine($"{v:e64}");
+            }
+
+            sw.WriteLine("u,expected,approx,error");
+            for (int i = 0; i < expecteds.Count; i++) {
+                sw.WriteLine($"{expecteds[i].u},{expecteds[i].v:e32},{approx[i]:e32},{(expecteds[i].v - approx[i].Convert<N32>()):e10}");
+            }
+        }
+
+        static List<(MultiPrecision<N> u, MultiPrecision<N> a)> ReadAExpected<N>(int n) where N : struct, IConstant {
+            List<(MultiPrecision<N> u, MultiPrecision<N> a)> res = new();
+
+            using StreamReader sr = new($"../../../../results/eigen_limit_precision64_n{n}.csv");
+            sr.ReadLine();
+            sr.ReadLine();
+            sr.ReadLine();
+
+            while (!sr.EndOfStream) {
+                string? line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) {
+                    break;
+                }
+
+                string[] line_split = line.Split(',');
+
+                MultiPrecision<N> u = line_split[0], a = line_split[1];
+
+                res.Add((u, a));
+            }
+
+            return res;
+        }
+
+        static List<(MultiPrecision<N> u, MultiPrecision<N> b)> ReadBExpected<N>(int n) where N : struct, IConstant {
+            List<(MultiPrecision<N> u, MultiPrecision<N> b)> res = new();
+
+            using StreamReader sr = new($"../../../../results/eigen_limit_precision64_n{n}.csv");
+            sr.ReadLine();
+            sr.ReadLine();
+            sr.ReadLine();
+
+            while (!sr.EndOfStream) {
+                string? line = sr.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) {
+                    break;
+                }
+
+                string[] line_split = line.Split(',');
+
+                MultiPrecision<N> u = line_split[0], b = line_split[2];
+
+                res.Add((u, b));
+            }
+
+            return res;
+        }
+
+        static MultiPrecision<N> LimitEigenA<N>(int n, MultiPrecision<N> q) where N : struct, IConstant {
+            int s = 2 * n + 1;
+            MultiPrecision<N> y = 2 * (s * MultiPrecision<N>.Sqrt(q) - q) - MultiPrecision<N>.Div(6 * n * n + 2 * n + 1, 4);
+
+            return y;
+        }
+
+        static MultiPrecision<N> LimitEigenB<N>(int n, MultiPrecision<N> q) where N : struct, IConstant {
+            int s = 2 * n - 1;
+            MultiPrecision<N> y = 2 * (s * MultiPrecision<N>.Sqrt(q) - q) - MultiPrecision<N>.Div(6 * n * n - 2 * n + 1, 4);
+
+            return y;
+        }
+
+        static (Vector<N> parameter, Vector<N> approx, bool success) PadeApproximate<N>(List<(MultiPrecision<N32> u, MultiPrecision<N32> v, MultiPrecision<N32> v_limit)> expecteds, int numer, int denom) where N : struct, IConstant {
+            Vector<N> y = ((Vector<N32>)expecteds.Select(expected => expected.v).ToArray()).Convert<N>();
+            Vector<N> v_limit = ((Vector<N32>)expecteds.Select(expected => expected.v_limit).ToArray()).Convert<N>();
+
+            bool[] needs_increase_weight(Vector<N> x, Vector<N> y_delta_quad) {
+                Vector<N> y_delta = Vector<N>.Func(y_delta_quad, v => MultiPrecision<N>.Sqrt(MultiPrecision<N>.Sqrt(v)));
+
+                Vector<N> y_approx = v_limit - y_delta;
+
+                Vector<N> error = Vector<N>.Func(y, y_delta, (expected, delta) => MultiPrecision<N>.Abs(delta / expected));
+
+                return error.Select(e => e.val.Exponent > -104).ToArray();
+            }
+
+            Vector<N> xs = expecteds.Select((item) => item.u.Convert<N>()).ToArray();
+            Vector<N> ys = expecteds.Select((item) => MultiPrecision<N>.Pow((item.v_limit - item.v).Convert<N>(), 4)).ToArray();
+            Vector<N> weights = xs.Select(x => 1 / (x.val)).ToArray();
+
+            AdaptivePadeFitter<N> fitter = new(xs, ys, numer, denom, intercept: 0);
+
+            (Vector<N> parameter, bool success) = fitter.ExecuteFitting(weights, needs_increase_weight);
+            Vector<N> approx = fitter.FittingValue(expecteds.Select((item) => item.u.Convert<N>()).ToArray(), parameter);
+
+            return (parameter, approx, success);
+        }
+    }
+}
