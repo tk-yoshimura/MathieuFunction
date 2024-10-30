4d639d89a7f98a2d3ffdd6984599b2172b0443f8
tk-yoshimura <yoshimura8192@gmail.com>
2024-10-30 22:47:49 +0900

    fix: digit145

--- MathieuDigits145/Program.cs
+++ MathieuDigits145/Program.cs
@@ -5,38 +5,38 @@ using static MultiPrecision.Pow2;
 namespace MathieuMP {
     class Program {
         static void Main() {
-            List<MultiPrecision<N8>> us = [];
+            List<MultiPrecision<N16>> us = [];
 
-            for (MultiPrecision<N8> u = MultiPrecision<N8>.Ldexp(1, -64); u < 1 / 1024d; u *= 2) {
+            for (MultiPrecision<N16> u = MultiPrecision<N16>.Ldexp(1, -64); u < 1 / 8192d; u *= 2) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 1 / 1024d; u < 1; u += 1 / 1024d) {
+            for (MultiPrecision<N16> u = 1 / 8192d; u < 1; u += 1 / 8192d) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 1; u < 4; u += 1 / 256d) {
+            for (MultiPrecision<N16> u = 1; u < 4; u += 1 / 2048d) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 4; u < 16; u += 1 / 64d) {
+            for (MultiPrecision<N16> u = 4; u < 16; u += 1 / 256d) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 16; u < 64; u += 1 / 16d) {
+            for (MultiPrecision<N16> u = 16; u < 64; u += 1 / 32d) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 64; u < 256; u += 1 / 4d) {
+            for (MultiPrecision<N16> u = 64; u < 256; u += 1 / 8d) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 256; u < 1024; u += 1) {
+            for (MultiPrecision<N16> u = 256; u < 1024; u += 1 / 2d) {
                 us.Add(u);
             }
-            for (MultiPrecision<N8> u = 1024; u <= 4096; u += 4) {
+            for (MultiPrecision<N16> u = 1024; u <= 4096; u += 2) {
                 us.Add(u);
             }
 
             for (int n = 0; n <= 16; n++) {
                 Console.WriteLine($"Plotting {n}");
 
-                using StreamWriter sw = new($"../../../../results/eigen_precision64_n{n}.csv");
-                sw.WriteLine($"# zero shifted mathieu eigen value precision_digits=64 n={n}");
+                using StreamWriter sw = new($"../../../../results/eigen_precision145_n{n}.csv");
+                sw.WriteLine($"# zero shifted mathieu eigen value precision_digits=145 n={n}");
 
                 if (n >= 1) {
                     sw.WriteLine("# u:=q^2/n^4, m:=(a+b-2n^2)/2n^2, d:=q^n/(2^(n-1)(n-1)!)^2 2/(a-b)-1");
@@ -52,18 +52,18 @@ namespace MathieuMP {
 
                 sw.WriteLine("0,0,0,0,0,0");
 
-                foreach (MultiPrecision<N8> u in us) {
+                foreach (MultiPrecision<N16> u in us) {
                     MultiPrecision<N64> q = s * MultiPrecision<N64>.Sqrt(u.Convert<N64>());
 
-                    (MultiPrecision<N8> a, MultiPrecision<N8> b, MultiPrecision<N8> m, MultiPrecision<N8> d, bool cancellation_digits, mp_length)
+                    (MultiPrecision<N16> a, MultiPrecision<N16> b, MultiPrecision<N16> m, MultiPrecision<N16> d, bool cancellation_digits, mp_length)
                         = ComputeDigits64(n, q, mp_length);
 
                     if (n >= 1) {
-                        sw.WriteLine($"{u},{a:e64},{b:e64},{m:e64},{d:e64},{(cancellation_digits ? "1" : "0")}");
+                        sw.WriteLine($"{u},{a},{b},{m},{d},{(cancellation_digits ? "1" : "0")}");
                         Console.WriteLine($"{u},{a:e20},{d:e20}");
                     }
                     else {
-                        sw.WriteLine($"{u},{a:e64},0,{a:e64},0,0");
+                        sw.WriteLine($"{u},{a},0,{a},0,0");
                         Console.WriteLine($"{u},{a:e20}");
                     }
 
@@ -113,120 +113,106 @@ namespace MathieuMP {
             return (a.Convert<N>(), b.Convert<N>(), m.Convert<N>(), d.Convert<N>(), cancellation_digits);
         }
 
-        static (MultiPrecision<N8> a, MultiPrecision<N8> b, MultiPrecision<N8> m, MultiPrecision<N8> d, bool cancellation_digits, int mp_length) ComputeDigits64(int n, MultiPrecision<N64> q, int mp_length) {
+        static (MultiPrecision<N16> a, MultiPrecision<N16> b, MultiPrecision<N16> m, MultiPrecision<N16> d, bool cancellation_digits, int mp_length) ComputeDigits64(int n, MultiPrecision<N64> q, int mp_length) {
             if (n == 0) {
-                return (EigenMP<N8>.Value(EigenFunc.A, n, q.Convert<N8>(), zero_shift: true).value, 0, 0, 0, cancellation_digits: false, mp_length: 0);
+                return (EigenMP<N16>.Value(EigenFunc.A, n, q.Convert<N16>(), zero_shift: true).value, 0, 0, 0, cancellation_digits: false, mp_length: 0);
             }
 
-            const int needs_bits = 228;
+            const int needs_bits = 484;
 
-            MultiPrecision<N8> a, b, m, d;
+            MultiPrecision<N16> a, b, m, d;
             bool cancellation_digits;
 
             if (mp_length <= 8) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, N8>(n, q.Convert<N8>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, N16>(n, q.Convert<N16>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 8);
                 }
             }
 
             if (mp_length <= 12) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus4<N8>>(n, q.Convert<Plus4<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus4<N16>>(n, q.Convert<Plus4<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 12);
                 }
             }
 
             if (mp_length <= 16) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus8<N8>>(n, q.Convert<Plus8<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus8<N16>>(n, q.Convert<Plus8<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 16);
                 }
             }
 
             if (mp_length <= 20) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus12<N8>>(n, q.Convert<Plus12<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus12<N16>>(n, q.Convert<Plus12<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 20);
                 }
             }
 
             if (mp_length <= 24) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus16<N8>>(n, q.Convert<Plus16<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus16<N16>>(n, q.Convert<Plus16<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 24);
                 }
             }
 
             if (mp_length <= 28) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus20<N8>>(n, q.Convert<Plus20<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus20<N16>>(n, q.Convert<Plus20<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 28);
                 }
             }
 
             if (mp_length <= 32) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus24<N8>>(n, q.Convert<Plus24<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus24<N16>>(n, q.Convert<Plus24<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 32);
                 }
             }
 
             if (mp_length <= 36) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus28<N8>>(n, q.Convert<Plus28<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus28<N16>>(n, q.Convert<Plus28<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 36);
                 }
             }
 
             if (mp_length <= 40) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus32<N8>>(n, q.Convert<Plus32<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus32<N16>>(n, q.Convert<Plus32<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 40);
                 }
             }
 
             if (mp_length <= 44) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus36<N8>>(n, q.Convert<Plus36<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus36<N16>>(n, q.Convert<Plus36<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 44);
                 }
             }
 
             if (mp_length <= 48) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus40<N8>>(n, q.Convert<Plus40<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus40<N16>>(n, q.Convert<Plus40<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 48);
                 }
             }
 
             if (mp_length <= 52) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus44<N8>>(n, q.Convert<Plus44<N8>>(), needs_bits);
+                (a, b, m, d, cancellation_digits) = Compute<N16, Plus44<N16>>(n, q.Convert<Plus44<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, b, m, d, cancellation_digits, 52);
                 }
             }
 
-            if (mp_length <= 56) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus48<N8>>(n, q.Convert<Plus48<N8>>(), needs_bits);
-                if (!cancellation_digits) {
-                    return (a, b, m, d, cancellation_digits, 56);
-                }
-            }
-
-            if (mp_length <= 60) {
-                (a, b, m, d, cancellation_digits) = Compute<N8, Plus52<N8>>(n, q.Convert<Plus52<N8>>(), needs_bits);
-                if (!cancellation_digits) {
-                    return (a, b, m, d, cancellation_digits, 60);
-                }
-            }
-
-            (a, b, m, d, cancellation_digits) = Compute<N8, N64>(n, q.Convert<N64>(), needs_bits);
+            (a, b, m, d, cancellation_digits) = Compute<N16, N64>(n, q.Convert<N64>(), needs_bits);
             if (!cancellation_digits) {
                 return (a, b, m, d, cancellation_digits, 64);
             }
 
-            (a, b, m, d, cancellation_digits) = Compute<N8, N128>(n, q.Convert<N128>(), needs_bits);
+            (a, b, m, d, cancellation_digits) = Compute<N16, N128>(n, q.Convert<N128>(), needs_bits);
             return (a, b, m, d, cancellation_digits, 64);
         }
     }
--- MathieuLimitDigits145/Program.cs
+++ MathieuLimitDigits145/Program.cs
@@ -4,29 +4,29 @@ using static MultiPrecision.Pow2;
 namespace MathieuMP {
     class Program {
         static void Main() {
-            List<MultiPrecision<N8>> fs = [];
+            List<MultiPrecision<N16>> fs = [];
 
-            for (MultiPrecision<N8> f = MultiPrecision<N8>.Ldexp(1, -40); f < MultiPrecision<N8>.Ldexp(1, -20); f *= 2) {
-                for (MultiPrecision<N8> r = 1; r < 2; r += 1d / 16) {
+            for (MultiPrecision<N16> f = MultiPrecision<N16>.Ldexp(1, -40); f < MultiPrecision<N16>.Ldexp(1, -20); f *= 2) {
+                for (MultiPrecision<N16> r = 1; r < 2; r += 1d / 64) {
                     fs.Add(f * r);
                 }
             }
-            for (MultiPrecision<N8> f = MultiPrecision<N8>.Ldexp(1, -20); f < MultiPrecision<N8>.Ldexp(1, -16); f *= 2) {
-                for (MultiPrecision<N8> r = 1; r < 2; r += 1d / 64) {
+            for (MultiPrecision<N16> f = MultiPrecision<N16>.Ldexp(1, -20); f < MultiPrecision<N16>.Ldexp(1, -16); f *= 2) {
+                for (MultiPrecision<N16> r = 1; r < 2; r += 1d / 256) {
                     fs.Add(f * r);
                 }
             }
-            for (MultiPrecision<N8> f = MultiPrecision<N8>.Ldexp(1, -16); f <= MultiPrecision<N8>.Ldexp(1, -12); f *= 2) {
-                for (MultiPrecision<N8> r = 1; r < 2; r += 1d / 64) {
+            for (MultiPrecision<N16> f = MultiPrecision<N16>.Ldexp(1, -16); f <= MultiPrecision<N16>.Ldexp(1, -12); f *= 2) {
+                for (MultiPrecision<N16> r = 1; r < 2; r += 1d / 256) {
                     fs.Add(f * r);
                 }
             }
 
-            for (int n = 0; n <= 16; n++) {
+            for (int n = 14; n <= 15; n++) {
                 Console.WriteLine($"Plotting {n}");
 
-                using StreamWriter sw = new($"../../../../results/eigen_limit_precision64_n{n}.csv");
-                sw.WriteLine($"# zero shifted mathieu eigen limit value precision_digits=64 n={n}");
+                using StreamWriter sw = new($"../../../../results/eigen_limit_precision145_n{n}.csv");
+                sw.WriteLine($"# zero shifted mathieu eigen limit value precision_digits=145 n={n}");
 
                 if (n >= 1) {
                     sw.WriteLine("# u:=q^2/n^4");
@@ -42,19 +42,19 @@ namespace MathieuMP {
 
                 sw.WriteLine("0,-inf,-inf,0,-inf,-inf,0,0");
 
-                foreach (MultiPrecision<N8> f in fs) {
+                foreach (MultiPrecision<N16> f in fs) {
                     MultiPrecision<N64> u = 1 / f.Convert<N64>();
                     MultiPrecision<N64> q = s * MultiPrecision<N64>.Sqrt(u);
 
-                    (MultiPrecision<N8> a, MultiPrecision<N8> a_limit, MultiPrecision<N8> a_delta, MultiPrecision<N8> b, MultiPrecision<N8> b_limit, MultiPrecision<N8> b_delta, bool cancellation_digits, mp_length)
+                    (MultiPrecision<N16> a, MultiPrecision<N16> a_limit, MultiPrecision<N16> a_delta, MultiPrecision<N16> b, MultiPrecision<N16> b_limit, MultiPrecision<N16> b_delta, bool cancellation_digits, mp_length)
                         = ComputeDigits64(n, q, mp_length);
 
                     if (n >= 1) {
-                        sw.WriteLine($"{f},{a:e64},{a_limit:e64},{a_delta:e64},{b:e64},{b_limit:e64},{b_delta:e64},{(cancellation_digits ? "1" : "0")}");
+                        sw.WriteLine($"{f},{a},{a_limit},{a_delta},{b},{b_limit},{b_delta},{(cancellation_digits ? "1" : "0")}");
                         Console.WriteLine($"{f},{a:e20},{a_limit:e20},{b:e20},{b_limit:e20}");
                     }
                     else {
-                        sw.WriteLine($"{f},{a:e64},{a_limit:e64},{a_delta:e64},0,0,0,0");
+                        sw.WriteLine($"{f},{a},{a_limit},{a_delta},0,0,0,0");
                         Console.WriteLine($"{f},{a:e20},{a_limit:e20}");
                     }
 
@@ -95,116 +95,102 @@ namespace MathieuMP {
                     b.Convert<N>(), b_limit.Convert<N>(), b_delta.Convert<N>(), cancellation_digits);
         }
 
-        static (MultiPrecision<N8> a, MultiPrecision<N8> a_limit, MultiPrecision<N8> a_delta, MultiPrecision<N8> b, MultiPrecision<N8> b_limit, MultiPrecision<N8> b_delta, bool cancellation_digits, int mp_length) ComputeDigits64(int n, MultiPrecision<N64> q, int mp_length) {
-            const int needs_bits = 228;
+        static (MultiPrecision<N16> a, MultiPrecision<N16> a_limit, MultiPrecision<N16> a_delta, MultiPrecision<N16> b, MultiPrecision<N16> b_limit, MultiPrecision<N16> b_delta, bool cancellation_digits, int mp_length) ComputeDigits64(int n, MultiPrecision<N64> q, int mp_length) {
+            const int needs_bits = 484;
 
-            MultiPrecision<N8> a, a_limit, a_delta, b, b_limit, b_delta;
+            MultiPrecision<N16> a, a_limit, a_delta, b, b_limit, b_delta;
             bool cancellation_digits;
 
             if (mp_length <= 8) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, N8>(n, q.Convert<N8>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, N16>(n, q.Convert<N16>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 8);
                 }
             }
 
             if (mp_length <= 12) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus4<N8>>(n, q.Convert<Plus4<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus4<N16>>(n, q.Convert<Plus4<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 12);
                 }
             }
 
             if (mp_length <= 16) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus8<N8>>(n, q.Convert<Plus8<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus8<N16>>(n, q.Convert<Plus8<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 16);
                 }
             }
 
             if (mp_length <= 20) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus12<N8>>(n, q.Convert<Plus12<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus12<N16>>(n, q.Convert<Plus12<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 20);
                 }
             }
 
             if (mp_length <= 24) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus16<N8>>(n, q.Convert<Plus16<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus16<N16>>(n, q.Convert<Plus16<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 24);
                 }
             }
 
             if (mp_length <= 28) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus20<N8>>(n, q.Convert<Plus20<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus20<N16>>(n, q.Convert<Plus20<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 28);
                 }
             }
 
             if (mp_length <= 32) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus24<N8>>(n, q.Convert<Plus24<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus24<N16>>(n, q.Convert<Plus24<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 32);
                 }
             }
 
             if (mp_length <= 36) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus28<N8>>(n, q.Convert<Plus28<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus28<N16>>(n, q.Convert<Plus28<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 36);
                 }
             }
 
             if (mp_length <= 40) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus32<N8>>(n, q.Convert<Plus32<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus32<N16>>(n, q.Convert<Plus32<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 40);
                 }
             }
 
             if (mp_length <= 44) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus36<N8>>(n, q.Convert<Plus36<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus36<N16>>(n, q.Convert<Plus36<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 44);
                 }
             }
 
             if (mp_length <= 48) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus40<N8>>(n, q.Convert<Plus40<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus40<N16>>(n, q.Convert<Plus40<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 48);
                 }
             }
 
             if (mp_length <= 52) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus44<N8>>(n, q.Convert<Plus44<N8>>(), needs_bits);
+                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, Plus44<N16>>(n, q.Convert<Plus44<N16>>(), needs_bits);
                 if (!cancellation_digits) {
                     return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 52);
                 }
             }
 
-            if (mp_length <= 56) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus48<N8>>(n, q.Convert<Plus48<N8>>(), needs_bits);
-                if (!cancellation_digits) {
-                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 56);
-                }
-            }
-
-            if (mp_length <= 60) {
-                (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, Plus52<N8>>(n, q.Convert<Plus52<N8>>(), needs_bits);
-                if (!cancellation_digits) {
-                    return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 60);
-                }
-            }
-
-            (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, N64>(n, q.Convert<N64>(), needs_bits);
+            (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, N64>(n, q.Convert<N64>(), needs_bits);
             if (!cancellation_digits) {
                 return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 64);
             }
 
-            (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N8, N128>(n, q.Convert<N128>(), needs_bits);
+            (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits) = Compute<N16, N128>(n, q.Convert<N128>(), needs_bits);
             return (a, a_limit, a_delta, b, b_limit, b_delta, cancellation_digits, 64);
         }
 
